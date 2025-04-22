8269e425a37cf93c4261cd83f1a1d96e9960f556
mdraihan51674 <mdraihan51674@gmail.com>
2025-04-23 00:01:42 +0600

    all

--- src/Pages/Services/Service.jsx
+++ src/Pages/Services/Service.jsx
@@ -6,7 +6,7 @@ const Service = () => {
     <div>
       <div>
       <h1 className='text-3xl text-center font-extrabold'>We Provide Best Law Services</h1>
-      <p className='text-center text-[14px] text-gray-400 pt-3'>Our platform connects you with verified, experienced Lawyers across various specialities — all at your convenience. </p>
+      <p className='text-center text-[14px] text-gray-400 pt-3'>Our platform connects you with verified, experienced Lawyers across various specialitie — all at your convenience. </p>
     </div>
     <ServiceCard></ServiceCard>
     </div>
